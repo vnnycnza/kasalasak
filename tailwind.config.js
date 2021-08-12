a4d7713659449ebaa5609367609357a657bc78a8
Vanny Conoza <vanny@pigeonlab.com>
2021-08-12 11:43:28 +0800

    Setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            display: ['Tenor Sans', 'Georgia', 'serif'],
+            body: ['Inter', 'system-ui', 'sans-serif']
+        }
+    }
+}
